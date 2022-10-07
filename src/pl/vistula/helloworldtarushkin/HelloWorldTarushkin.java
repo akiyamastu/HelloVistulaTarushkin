3f3443190d86946e401d8ff81dc69b6fe68df74e
Your Name <you@example.com>
2022-10-07 14:25:05 +0200

    git branch 2 commit

--- src/pl/vistula/helloworldtarushkin/HelloWorldTarushkin.java
+++ src/pl/vistula/helloworldtarushkin/HelloWorldTarushkin.java
@@ -3,7 +3,6 @@ package pl.vistula.helloworldtarushkin;
 public class HelloWorldTarushkin {
     public static void main(String[] args) {
         System.out.println("Hello Vistula Tarushkin");
-
         System.out.println("Hello Vistula Tarushkin2");
 
     }
